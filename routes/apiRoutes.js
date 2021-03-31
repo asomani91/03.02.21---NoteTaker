ea2ca4f16ffcd31ca0b2b329e8814c8b076d9965
Ankit Somani <ankit.m.somani@gmail.com>
2021-03-31 12:29:13 -0400

    creating post/delete requests in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,19 +3,24 @@ const router = require('express').Router();
 const store = require('../db/store')
 
 // make a GET request with all notes from the database
-
 router.get('/notes', (req, res) => {
     store
-    .getNotes()
-    .then((notes) => {
-        return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err));
 })
 
 // create a post request
+router.post("/notes", (req, res) => {
+    console.log(req.body)
+    store.j(req.body)
+})
 
 // create a delete request
-
+router.delete("/notes", (req, res) => {
+    console.log(req.body)
+})
 
 module.exports = router;
