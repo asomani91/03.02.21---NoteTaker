66b52583f709865737029397093a84136b7ee5f9
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-18 18:35:42 -0400

    adding htmlRoutes foundation

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,3 +3,22 @@ const router = require('express').Router();
 const store = require('../db/store')
 
 // MAKE A GET REQUEST WITH ALL NOTES FROM THE DB //
+
+module.exports = (app) => {
+    // => HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+  
+    app.get('/tables', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/tables.html'));
+    });
+  
+    app.get('/reserve', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/reserve.html'));
+    });
+  
+    // If no matching route is found default to home
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+  };
